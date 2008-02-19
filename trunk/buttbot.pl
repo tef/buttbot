501d16f3aae0e90b84106ede1741072e2d187826
NZatkovich <NZatkovich@cac83df2-fa40-0410-8867-2b7918b5bb02>
2008-02-19 13:05:13 +0000

    Lots of changes: Changed admin functionality. Only works through PMs now. New admin commands: 	!quote: sends string directly to sever 	!echo: sends string to current configured channel 	!echobutt: same as above, but buttifies message 	!normfreq: changes the frequency normal people get butted 	!enemfreq: changes the freqeuncy enemies get butted 	!addfriend: Friends don't get butted at all. 	!remfriend: 	!addenemy: Enemies get butted more often 	!remenemy:
    	adding the word "loud" to the end of an !addfriend, et.al 
    command will announce the change publicly in channel.
    
    Added aformentioned Friends and Enemies lists.
    Added variable frequency of butting.
    Added PM support. If somebody PM's buttbot with a !butt command, he'll 
    respond.
    
    --Doctor Nick

--- trunk/buttbot.pl
+++ trunk/buttbot.pl
@@ -6,7 +6,7 @@ use Butts qw(buttify);
 use IO::Socket;
 
 ## globals
-use vars qw<$sock %CONF %results $hyp>;
+use vars qw/$sock %CONF %results $hyp/;
 $|=1;
 
 $CONF{file} = shift;
@@ -29,8 +29,17 @@ my ($auth, @buffer) ;
 $auth ="";
 @buffer=();
 my ($from,$command,@data);
+#list of enemies (people who get buttified more often) and friends (people who dont get butted.)
+my (@enemies, @friends);
+#frequency that normal people and enemies get butted
+my ($normalfrequency, $enemyfrequency);
+#last thing said by someone in the channel
+my (@previousdata);
+#pre-setting frequencies
+$enemyfrequency = 23;
+$normalfrequency = 37;
 
-
+#main execution loop
 while (1) {
 
   &error("main: $! $@") if (($! ne "" ) || ($@ ne ""));
@@ -43,7 +52,7 @@ while (1) {
 
    $from ||= '';
    $command ||= '';
-
+   #if server pings, ping back.
    if ($from eq "PING") {
 	   if ($command=~/^:\d+$/) {
 	   		&send("PONG $command");
@@ -54,42 +63,187 @@ while (1) {
    
    &error("from server: @data") if ($from eq "ERROR");
  
+  #If buttbot has successfully connected to the server, join a channel.
    if ($command eq "001") {
-     &send("JOIN $CONF{channel}") if (defined $CONF{channel});
-   } elsif ($command eq "PRIVMSG") {
-      my $to=shift(@data);
-      my $sub=shift(@data);
-     $sub=~s/^://;
-     
-      if ($sub eq $CONF{pass}) {
-         $auth=$from;
+     if (defined $CONF{channel})
+	{
+		&send("JOIN $CONF{channel}") ;
+		#&send("PRIVMSG $CONF{channel} : BUTTING SYSTEMS ONLINE!");
 	}
-      
-      if ($auth eq $from)  {
-	    &send(@data) if ($sub eq "quote");
-      }
-      if ($to =~ /^#/)  {
+   } 
+	#otherwise, if it's a message
+	elsif ($command eq "PRIVMSG") {
+	#get destination of message
+      my $to=shift(@data);
+	#get first word of message (might be command)
+      my $sub=shift(@data);
+     ## remove preceding ':'
+	$sub=~s/^://;
+
+	##if a user private messages the bot...
+	if ($to eq $CONF{nick})
+	{
+		$to = $from;
+		$to =~ s/^:(.*)!.*$/$1/;
+		#If the command is !butt, buttify message.
+		if ($sub eq "!butt" and @data >0 ) 
+		{
+	     		 if (($data[0] !~ /^!/) && ($data[0] !~ /^cout/)) 
+			{
+				  &send("PRIVMSG $to :".join(" ",&buttify(@data)));
+			}
+		}
+		##if the first word in the string is equal to the password, set the user to be the admin
+		if ($sub eq $CONF{pass}) {
+		$auth=$from;
+		}
+		##ADMIN FUNCTIONS
+		 if ($auth eq $from)  {
+		##if the first word is "!quote", send the string that follows to the server
+		## e.g. "quote PRIVMSG #testing : HELLO" prints out "HELLO" to #testing
+			if ($sub eq "!quote" and @data >0 )
+			{
+				&send(@data) ;
+			}
+		##!echo spits out whatever to the channel
+			if ($sub eq "!echo" and @data >0 )
+			{
+				&send("PRIVMSG $CONF{channel} :".join(" ",@data));
+			}
+		##!echobutt spits out whatever to the channel, but will buttify it
+			if ($sub eq "!echobutt" and @data >0 )
+			{
+				&send("PRIVMSG $CONF{channel} :".join(" ",&buttify(@data)));
+			}
+		##!normfreq changes the frequency the normal people get butted
+			if ($sub eq "!normfreq" and @data >0 )
+			{
+				$normalfrequency = $data[0];
+				print("Normal Frequency changed to $normalfrequency");
+			}
+		##!enemfreq changes the frequency the enemies get butted
+			if ($sub eq "!enemfreq" and @data >0 )
+			{
+				$enemyfrequency = $data[0];
+				print("Enemy Frequency changed to $enemyfrequency");
+			}
+		##!addenemy adds someone to the enemy list. 
+			if ($sub eq "!addenemy" and @data >0 )
+			{
+				push(@enemies, $data[0]);
+				printf("Enemies:\n");
+				foreach (@enemies)
+				{
+					printf("$_\n");
+				}
+				if (@data >1)
+				{
+					if ($data[1] eq "loud")
+					{
+						&send("PRIVMSG $CONF{channel} : TARGET DESIGNATED: $data[0]");
+					}
+				}
+			}
+		##!remenemy removes someone from the enemy list
+			if ($sub eq "!remenemy" and @data >0 )
+			{
+				removeenemy($data[0]);
+				printf("Enemies:\n");
+				foreach (@enemies)
+				{
+					printf("$_\n");
+				}
+				if (@data >1)
+				{
+					if ($data[1] eq "loud")
+					{
+						&send("PRIVMSG $CONF{channel} : TARGET REMOVED: $data[0]");
+					}
+				}
+			}
+		##!addfriend adds someone to the friend list
+			if ($sub eq "!addfriend" and @data >0 )
+			{
+				push(@friends, $data[0]);
+				printf("Friends:\n");
+				foreach (@friends)
+				{
+					printf("$_\n");
+				}
+				if (@data >1)
+				{
+					if ($data[1] eq "loud")
+					{
+						&send("PRIVMSG $CONF{channel} : $data[0], you're my bff :)");
+					}
+				}
+			}
+		##!remfriend removes someone from the friend list
+			if ($sub eq "!remfriend" and @data >0 )
+			{
+				removefriend($data[0]);
+				printf("Friends:\n");
+				foreach (@friends)
+				{
+					printf("$_\n");
+				}
+				if (@data >1)
+				{
+					if ($data[1] eq "loud")
+					{
+						&send("PRIVMSG $CONF{channel} : $data[0],  I'm breaking up with you :(");
+					}
+				}
+			}
+		##!buttnow will buttify the previous message said in the channel.
+			if ($sub eq "!buttnow" and @previousdata > 0)
+			{
+				if (($previousdata[0] !~ /^!/) && ($previousdata[0] !~ /^cout/)) 
+				{
+			  		&send("PRIVMSG $CONF{channel} :".join(" ",&buttify(@previousdata)));
+				}
+			}
+		
+			
+		}
+	}
+	#if messages come from channel, start buttifying
+      elsif ($to =~ /^#/ )  {
+		
+		##ignores statements from cout and users containing the word "bot"
               if (($from !~/^:cout/) && ($from !~/^:[^!]*bot[^!]*!/i)) {
 	      if ($sub !~ /^!/) {
 			my $rnd = 1;
-			if (@data > 2) {
-			if ($from =~ /floWenoL/ ) { 
-			  $rnd = int(rand(23));
-			} else {
-			   $rnd = int(rand(37));
+			if (@data > 1) {
+				#if it's a friend, don't buttify message. If enemy, buttify message more often.
+				if (isfriend($from)) {
+				$rnd = 1;
+				}
+				elsif (isenemy($from)) { 
+				$rnd = int(rand(int($enemyfrequency)));
+				} 
+				else {
+				$rnd = int(rand(int($normalfrequency)));
+				}
+				
 			}
-			}
-			if ($rnd ==0) {
 			  unshift (@data,$sub);
-			  sleep(@data*0.2+1);
+			#if the random number is 0, buttify that data
+			if ($rnd ==0) {
+			  sleep(@data*0.2 + 1);
 			  &send("PRIVMSG $to :".join(" ",&buttify(@data)));
+			}
+			#store this for later butting
+			else
+			{
+				@previousdata = @data;
 			}
 	      } elsif ($sub eq "!butt" and @data >0 ) {
 	          if (($data[0] !~ /^!/) && ($data[0] !~ /^cout/)) {
 		  &send("PRIVMSG $to :".join(" ",&buttify(@data)));
 	      }
 	      }
-}
+	 }
 	 }
    }
  }
@@ -135,6 +289,63 @@ my $spoon=fork();
 sub error {
     print "\nerror: @_\n";
     exit;
+}
+
+sub isenemy{
+	my($victim, $enemyname);
+	$victim = $_[0];
+	$victim =~ s/^:(.*)!.*$/$1/;
+	foreach $enemyname (@enemies)
+	{
+		if ( $victim eq $enemyname)
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
+
+sub isfriend{
+	
+	my($victim, $friendname);
+	$victim = $_[0];
+	$victim =~ s/^:(.*)!.*$/$1/;
+	foreach $friendname (@friends)
+	{
+		if ( $victim eq $friendname)
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
+sub removeenemy{
+	my($victim, $blargh, $enemyname);
+	$victim = $_[0];
+	$blargh = 0;
+	foreach $enemyname (@enemies)
+	{
+		if ( $victim eq $enemyname)
+		{
+			$enemies[$blargh] = "";
+		}
+		$blargh++;
+	}
+	
+}
+
+sub removefriend{
+	my($victim, $blargh, $friendname);
+	$victim = $_[0];
+	$blargh = 0;
+	foreach $friendname (@friends)
+	{
+		if ( $victim eq $friendname)
+		{
+			$friends[$blargh] = "";
+		}
+		$blargh++;
+	}
 }
 
 sub readconf {
