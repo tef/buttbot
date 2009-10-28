363e6618821a377f1736080714bad1b82c5bde56
shabble <shabble@cac83df2-fa40-0410-8867-2b7918b5bb02>
2009-10-28 18:38:10 +0000

    fixed the bogocheck, added some more tests, and cleaned up buttest some more

--- trunk/buttest.pl
+++ trunk/buttest.pl
@@ -1,14 +1,20 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 use strict;
 use warnings;
 
 use Butts;
 
-my $butt = $ARGV[0] || "butt";
-my $buttifier = Butts->new(meme => $butt, debug => 1,
-                            replace_freq => $ARGV[1] // 0.5);
+my $butt = $ARGV[0]         // "butt";
+my $replace_freq = $ARGV[1] // 0.5;
+my $debug = $ARGV[2]        // 0;
 
-print STDERR "butt repeat rate is " . $buttifier->{replace_freq} . $/;
+my $buttifier = Butts->new(meme         => $butt,
+                           debug        => $debug,
+                           replace_freq => $replace_freq);
+
+print STDERR "$butt repeat rate is " . $buttifier->replace_freq . $/
+  if $debug;
+
 while(<STDIN>) {
 
     # The old way
